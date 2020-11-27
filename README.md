1959a4029a6737a9ecd3526a0265ff9567ea5124
Daksh C Reddy <70056557+dakshreddy@users.noreply.github.com>
2020-11-27 21:56:21 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# pliku-game
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,26 @@
+var d1,d2,d3,d4,d5,d6,d7;
+
+
+
+function setup() {
+  createCanvas(480,800);
+    d1 = new Divisions(100,100,10,10);
+    d2 = new Divisions(200,100,10,10);
+    d3 = new Divisions(300,100,01,10);
+    d4 = new Divisions(400,100,10,10);
+    d5 = new Divisions(500,100,10,10);
+    d6 = new Divisions(600,100,10,10);
+    d7 = new Divisions(700,100,10,10);
+   
+ 
+ 
+  
+}
+
+function draw() {
+  background("black");  
+
+
+
+  drawSprites();
+}
